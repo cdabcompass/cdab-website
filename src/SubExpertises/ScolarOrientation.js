01a4fab701a1929d0dcf8f5db7dbc41833c8b1d1
fabrice <fabricesumsa2000@gmail.com>
2021-01-13 08:36:08 +0100

    add image

--- src/SubExpertises/ScolarOrientation.js
+++ src/SubExpertises/ScolarOrientation.js
@@ -29,7 +29,7 @@ const styles = theme => ({
             backgroundColor: "#E58F1E",
         },
     },
-    profilImage:{
+    profilImage: {
         width: "100%",
         height: "500px",
         filter: "blur(2px)",
@@ -37,10 +37,9 @@ const styles = theme => ({
 });
 
 class ScolarOrientation extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
     render() {
@@ -61,9 +60,9 @@ class ScolarOrientation extends Component {
                 <div>
                     <MenuBar/>
                 </div>
-                <div style={{marginTop: "100px",marginBottom: "100px"}}>
+                <div style={{marginTop: "100px", marginBottom: "100px"}}>
                     <DetailsPageBanner
-                        image={<img className={classes.profilImage}  src={require("../assets/P3.JPG")} alt=""/>}
+                        image={<img className={classes.profilImage} src={require("../assets/P3.JPG")} alt=""/>}
                         title={"Accompagnement et orientation scolaire"}/>
                     <div className={classes.subContext}>
                         <div className="subContainer">
@@ -71,30 +70,46 @@ class ScolarOrientation extends Component {
                                 <iframe width="" height="186" src="https://www.youtube.com/embed/cML9g49ECVc"
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                        allowFullScreen></iframe></div>
+                                        allowFullScreen></iframe>
+                            </div>
                             <div style={{flex: 1}}>
-                                <p style={{fontSize: "20px",textAlign:"justify"}}>
-                                    La structure éducative CDAB COMPASS, spécialisée aussi dans l’accompagnement et l’orientation des apprenants sur tous les plans scolaire, universitaire et même professionnel,
+                                <p style={{fontSize: "20px", textAlign: "justify"}}>
+                                    La structure éducative CDAB COMPASS, spécialisée aussi dans l’accompagnement et
+                                    l’orientation des apprenants sur tous les plans scolaire, universitaire et même
+                                    professionnel,
                                     accorde une importance à cette expertise.
-                                    Ainsi, CDAB COMPASS la propose afin d’aider les apprenants à renforcer leur niveau et à bien s’orienter pour atteindre leur objectif professionnel.
+                                    Ainsi, CDAB COMPASS la propose afin d’aider les apprenants à renforcer leur niveau
+                                    et à bien s’orienter pour atteindre leur objectif professionnel.
 
-                                    <br/><br/>L’accompagnement est un point essentiel pour permettre à l’apprenant d’être encrer plus solidement dans son projet d’étude.
-                                    Il offre l’opportunité à l’apprenant de découvrir d’autres méthodes de faire les cours, et de rehausser son niveau dans les matières dites difficiles
+                                    <br/><br/>L’accompagnement est un point essentiel pour permettre à l’apprenant
+                                    d’être encrer plus solidement dans son projet d’étude.
+                                    Il offre l’opportunité à l’apprenant de découvrir d’autres méthodes de faire les
+                                    cours, et de rehausser son niveau dans les matières dites difficiles
                                     ou généralement appelées « bêtes noires ».
 
-                                    Par le biais de cette phase les apprenants reçoivent des cours de soutien ou de répétions appropriés dans chaque matière ou dans les matières dont
-                                    les besoins se font plus sentir. Ils ont selon leurs besoins un quota d’heures à faire. Cet accompagnement spécialisé sur le profil de l’apprenant permet
-                                    à ce dernier d’être recadré d’où le lien intrinsèque entre l’accompagnement et l’orientation.
+                                    Par le biais de cette phase les apprenants reçoivent des cours de soutien ou de
+                                    répétions appropriés dans chaque matière ou dans les matières dont
+                                    les besoins se font plus sentir. Ils ont selon leurs besoins un quota d’heures à
+                                    faire. Cet accompagnement spécialisé sur le profil de l’apprenant permet
+                                    à ce dernier d’être recadré d’où le lien intrinsèque entre l’accompagnement et
+                                    l’orientation.
 
-                                    <br/><br/>L’orientation consiste à soit restructurer l’apprenant soit à lui offrir d’autres opportunités insoupçonnées ainsi à le rediriger vers d’autres voies propices
-                                    à un meilleur avenir. Par conséquent, un apprenant ayant les capacités mécaniques peut être orienté vers l’ingénierie alors qu’il prévoyait être enseignant.
-                                    L’orientation peut permettre à un apprenant ou enfant non motivé à trouver ses repères pour une réussite professionnelle.
-                                    L’accompagnement et l’orientation scolaires conduisent à l’élaboration d’un projet d’étude adéquat.
+                                    <br/><br/>L’orientation consiste à soit restructurer l’apprenant soit à lui offrir
+                                    d’autres opportunités insoupçonnées ainsi à le rediriger vers d’autres voies
+                                    propices
+                                    à un meilleur avenir. Par conséquent, un apprenant ayant les capacités mécaniques
+                                    peut être orienté vers l’ingénierie alors qu’il prévoyait être enseignant.
+                                    L’orientation peut permettre à un apprenant ou enfant non motivé à trouver ses
+                                    repères pour une réussite professionnelle.
+                                    L’accompagnement et l’orientation scolaires conduisent à l’élaboration d’un projet
+                                    d’étude adéquat.
 
                                 </p>
                             </div>
                         </div>
-                        <Button onClick={()=>{window.location.href = "/expertises/profil_etudiant"}} className={classes.btnQuiz} >
+                        <Button onClick={() => {
+                            window.location.href = "/expertises/profil_etudiant"
+                        }} className={classes.btnQuiz}>
                             Passer le test
                         </Button>
                     </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Expertises from "./subMenu/Expertises";
