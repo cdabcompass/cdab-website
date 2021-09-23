25e67823fb5423dd6083ec9f19e5ff4aa1f82642
Fabrice-Fabio <35635121+Fabrice-Fabio@users.noreply.github.com>
2021-09-23 21:30:09 +0200

    Add image to articleData

--- src/articlesData/articleData.js
+++ src/articlesData/articleData.js
@@ -5,6 +5,7 @@ export const FrenshData = [
         "est dû au projet d’étude presque inexistant ou encore mal élaboré. " +
         "Car beaucoup  se lancent dans les études sans une vision globale.",
         link: "/actu/1",
+        image: "https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg"
     },
     {
         title: "La motivation : paramètre utile de réussite",
@@ -12,6 +13,7 @@ export const FrenshData = [
         " commencer un projet sans le terminer.  L’absence de motivation en " +
         "est l’une des raisons. Cela arrive à tout un chacun.",
         link: "/actu/2",
+        image: "https://cdn.pixabay.com/photo/2016/11/28/21/20/happiness-1866081__480.jpg"
     },
     {
         title: 'L’importance d’une bonne orientation',
@@ -19,6 +21,7 @@ export const FrenshData = [
         "professionnels qui sont eux-mêmes le bilan du vécu, de la personnalité, des " +
         "intérêts, des aptitudes, des motivations et des valeurs. Toute prise",
         link: "/actu/3",
+        image: "https://cdn.pixabay.com/photo/2016/10/19/18/32/compass-1753659__340.jpg"
     },
     {
         title: "Examens de fin d’année scolaire 2020 : Côte d'ivoire",
@@ -26,12 +29,14 @@ export const FrenshData = [
         "faible niveau scolaire des élèves.",
         link: "/actu/4",
         content: "bonjour",
+        image: "https://cdn.pixabay.com/photo/2020/07/05/10/00/student-5372393__340.jpg"
     },
     {
         title: 'Le profil de l’apprenant, une nécessité pour votre enfant',
         description: "Nous n’apprenons pas tous de la même manière. Notre personnalité "+
         "et notre entourage ont un impact direct sur nos façons d’appréhender, d’apprendre et d’assimiler les choses.",
         link: "/actu/5",
+        image: "https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396__340.jpg"
     }
 ]
 
@@ -41,33 +46,38 @@ export const EnglishData = [
         description: "Most of the time, young learners fail " +
         "is due to the study project almost non-existent or still poorly developed." +
         "Because many embark on studies without a global vision.",
-        link: "/actu/1"
+        link: "/actu/1",
+        image: "https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg"
     },
     {
         title: "Motivation: a useful parameter of success",
         description: "One of the most common causes of failure is" +
         " start a project without completing it. The lack of motivation in" +
         "is one of the reasons. It happens to everyone",
-        link: "/actu/2"
+        link: "/actu/2",
+        image: "https://cdn.pixabay.com/photo/2016/11/28/21/20/happiness-1866081__480.jpg"
     },
     {
         title: 'The importance of a good orientation',
         description: "A professional project is defined according to the objectives" +
         "professionals who are themselves the balance sheet of experience, personality," +
         "interests, skills, motivations and values. Any take",
-        link: "/actu/3"
+        link: "/actu/3",
+        image: "https://cdn.pixabay.com/photo/2016/10/19/18/32/compass-1753659__340.jpg"
     },
     {
         title: "2020 end of school year exams: Ivory Coast",
         description: "Proclamation of end-of-year exam results: pass rates proof of"+
         "low educational level of the pupils.",
-        link: "/actu/4"
+        link: "/actu/4",
+        image: "https://cdn.pixabay.com/photo/2020/07/05/10/00/student-5372393__340.jpg"
     },
     {
         title: 'The learner profile, a necessity for your child',
         description: "We do not all learn in the same way. Our personality" +
         "and those around us have a direct impact on the way we view, learn and assimilate things.",
-        link: "/actu/5"
+        link: "/actu/5",
+        image: "https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396__340.jpg"
     }
     
 ]
