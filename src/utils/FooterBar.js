cf4ca14ee708882dadf1defa32266f0b49da3c33
Charmidez Kodjo Assiobo <kodjoassiobo@protonmail.com>
2021-11-04 23:54:55 +0000

    Footer en cours

--- src/utils/FooterBar.js
+++ src/utils/FooterBar.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import Toolbar from "@material-ui/core/Toolbar";
-import AppBar from "@material-ui/core/AppBar/AppBar";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import {withStyles} from "@material-ui/core";
 
 const styles = theme => ({
@@ -11,6 +13,14 @@ const styles = theme => ({
     }
 });
 
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
 class FooterBar extends Component {
     constructor(props){
         super(props);
@@ -18,6 +28,11 @@ class FooterBar extends Component {
             isAdmin: false
         }
     }
+
+
+
+
+
 
     componentDidMount() {
         if(localStorage.getItem("firstName") === "admin"
@@ -33,60 +48,77 @@ class FooterBar extends Component {
         return (
             <div className="footBar">
                 <div className="footer">
-                    <div className="siegeSocial">
-                        <h5 className="footerTitle">SIEGE SOCIAL</h5>
-                        <p>
-                            1, Place de la Gare<br/>
-                            59000 Lille<br/>
-                            +33 (0) 3 59 82 54 92<br/>+33 (0) 6 71 33 37 06
-                        </p>
+                <Grid container spacing={1} justify="flex-end" direction="row">
+                    
+                    <div>
+
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid item xs={6}>
+                            <Item>
+                                <div className="siegeSocial">
+                                    <h5 className="footerTitle">FRANCE (SIEGE SOCIAL)</h5>
+                                    <p>
+                                        1, Place de la Gare<br/>
+                                        59000 Lille<br/>
+                                        +33 (0) 3 59 82 54 92<br/>+33 (0) 6 71 33 37 06
+                                    </p>
+                                </div>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Item>
+                                <div className="siegeSocial">
+                                    <h5 className="footerTitle">CÔTE D'IVOIRE</h5>
+                                    <p>
+                                    Abidjan XL Plateau Centre<br/>
+                                    Avenue Dr. CROZET, Immeuble XL,<br/>
+                                    6ème & 7 ème  étages, Plateau Abidjan,<br/>
+                                    01 BP 2785 Abidjan 01, Cote d’Ivoire +225 84 94 89 25<br/>
+                                    
+                                    </p>
+                                </div>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Item>
+                                <div className="siegeSocial">
+                                    <h5 className="footerTitle">SENEGAL</h5>
+                                    <p>
+                                    1st Floor SIA Building, Route de Ngor<br/>
+                                    Les Almadies, Dakar<br/>
+                                    14790<br/>
+                                    +221 777 52 53 01
+                                    </p>
+                                </div>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Item>
+                                <div className="siegeSocial">
+                                    <h5 className="footerTitle">TOGO</h5>
+                                    <p>
+                                    Derrière la Polyclinique Saint Joseph,<br/>
+                                    Quartier Hédzranawoé<br/>
+                                    +228 91 38 49 12<br/>+228 96 35 53 07
+                                    </p>
+                                </div>
+                            </Item>
+                        </Grid>
+                        
+                    </Grid>
+
                     </div>
-                    <div className="siegeSocial">
-                        <h5 className="footerTitle">CÔTE D'IVOIRE</h5>
-                        <p>
-                            Abidjan XL Plateau Centre<br/>
-                            Avenue Dr. CROZET, Immeuble XL,<br/>
-                            6ème & 7 ème  étages, Plateau Abidjan,<br/>
-                            01 BP 2785 Abidjan 01, Cote d’Ivoire<br/>
-                            +225 84 94 89 25
-                        </p>
+
+                    <div>
+                        <p>Un paragraphe</p>
+
                     </div>
-                    <div className="siegeSocial">
-                        <h5 className="footerTitle">SENEGAL</h5>
-                        <p>
-                            1st Floor SIA Building, Route de Ngor<br/>
-                            Les Almadies, Dakar<br/>
-                            14790<br/>
-                            +221 777 52 53 01
-                        </p>
-                    </div>
-                    <div className="siegeSocial">
-                        <h5 className="footerTitle">TOGO</h5>
-                        <p>
-                            Derrière la Polyclinique Saint Joseph,<br/>
-                            Quartier Hédzranawoé<br/>
-                            +228 91 38 49 12<br/>+228 96 35 53 07
-                        </p>
-                    </div>
-                    <div className="siegeSocial">
-                        <h5 className="footerTitle">Contact</h5>
-                        <p>
-                            contact@cdabcompass.com
-                        </p>
-                        {this.state.isAdmin && (
-                            <div className={classes.adminColor}
-                                onClick={()=>{window.location.href = '/panelAdmin'}}>
-                                Gestions des utilisateurs
-                            </div>
-                        )}
-                        {localStorage.getItem("situation")==="IntervenantCdab" && (
-                            <div className={classes.adminColor}
-                                 onClick={()=>{window.location.href = '/teacherPanel'}}>
-                                Rapport
-                            </div>
-                        )}
-                    </div>
-                </div>
+                  
+
+
+
+                </Grid>
+                </div>      
             </div>
         );
     }
