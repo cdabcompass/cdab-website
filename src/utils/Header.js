7bd9021b7375f05e6e017b910cdefa6aa1ab7a17
Theophile <lasshyon@yahoo.fr>
2021-11-07 11:11:39 +0000

    Menu en Majuscule

--- src/utils/Header.js
+++ src/utils/Header.js
@@ -114,7 +114,7 @@ changeLangue = (lg) =>{
           <Button style = {{textTransform: 'none', fontSize: '16px',  color : 'white'}} href="/expertises">{translate("Nos_expertises")}</Button>
           <Button style = {{textTransform: 'none', fontSize: '16px',  color : 'white'}} href="/training">{translate("Training")}</Button>
           <Button style = {{textTransform: 'none', fontSize: '16px',  color : 'white'}} href="/coaching">{translate("Coach_cdab")}</Button>
-          <Button style = {{textTransform: 'none', fontSize: '16px',  color : 'white'}} href="#">Contact</Button>
+          <Button style = {{textTransform: 'none', fontSize: '16px',  color : 'white'}} href="/nos_realisations">{translate("Nos_realisations")}</Button>
           <Button style =  {{background : '#e59033', color : 'white', borderRadius: 20 }} href="/connexion"><AccountCircleIcon/>{translate("Connexion")}</Button>
 
           </Grid>
